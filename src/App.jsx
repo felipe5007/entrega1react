6a89a5318ef229031bb25e19b3cc5cd3875c9839
Felipee <felipecalvo6@gmail.com>
2023-03-25 14:50:56 -0300

    carpeta apis

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   // El cart Widget esta dentro del Sidebar ultima lista, aun estoy repasando Tailwind y no se como dentro del nav desde APP
   return (
         <div className="flex">
-          <SideBar />
+          <SideBar />  
           <ItemListContainer habla ="Este es mi proyecto"/>
           </div>
   );
