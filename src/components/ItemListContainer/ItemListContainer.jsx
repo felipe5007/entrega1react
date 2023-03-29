b9a9bfee8edf046d430a56a5af1cd4efd4f1fd53
Felipee <felipecalvo6@gmail.com>
2023-03-29 19:28:44 -0300

    p2 avance 1.1

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,34 +4,30 @@ import { getProducts } from "../../asyncMock";
 const ItemListContainer = () => {
   const [products, setProducts] = useState([])
 
-useEffect(() => {
-  getProducts()
+useEffect(() =()
   .then(response => {
     setProducts(response)
   })
 }, [])
 
-return <h1>Cargando......</h1>
+return (
+      
+  <div>
+    <h1>Saludo</h1>
 
-    return (
-      <div> 
-        <h1>saludo</h1>
-        <div>
-        {
-        products.map(prod => {
-            return (
-              <div  key={prod.id} >
-              <h2>{prod.name}</h2>
-              <img src={prod.img} alt={prod.name} {Style={{width:100}}/>
-              <button>Ver Detalles</button>
-
-              </div>
-            )
-          }
-        }
+    <div>
+      {products.map((prod) => (
+        <div key={prod.id}>
+          <h2>{prod.name}</h2>
+          <img src={prod.img} alt={prod.name} />
+          <button>Ver Detalles</button>
         </div>
+      ))}
+    </div>
+  </div>
+
+)
     
-    )
 }
 
 export default ItemListContainer;
