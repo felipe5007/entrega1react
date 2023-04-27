4a98135729dbc53165e9402f02fe148ff038656f
Felipee <felipecalvo6@gmail.com>
2023-04-27 02:29:58 -0400

    pf 2

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 
-const ItemCount = ({stock, initial}) => {
+const ItemCount = ({stock, initial, onAdd}) => {
     const [count,setCount] = useState(initial)
 
 
@@ -10,15 +10,18 @@ const ItemCount = ({stock, initial}) => {
     }
 
     const decrement = () => {
-        if (count > 1) setCount(prev => prev - 1)
+        if (count > 1) setCount(prev => prev  - 1)
     }
+
+
+
 
     return (
         <div>
             <h1>{count}</h1>
             <button onClick={decrement}  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">-</button>
             <button onClick={increment} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+</button>
-            <button onClick={() => onAdd(count)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Agregar al carrito</button>
+            <button disabled={stock <= 0} onClick={() => onAdd(count)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Agregar al carrito</button>
         </div>
     )
 }
